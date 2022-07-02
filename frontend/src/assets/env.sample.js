d5c0462978961e24c748ed1e6ef2c2bdc277531a
BrendanMeins <brendan.27@hotmail.de>
2022-07-02 17:36:19 +0200

    deleted env.js, adjustments to environment.ts

--- frontend/src/assets/env.sample.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-  window["env"] = window["env"] || {};
-
-  // Environment variables
-  window["env"]["API_URL"] = "${API_URL}";
-  window["env"]["API_PORT"] = "${API_PORT}";
-})(this);
