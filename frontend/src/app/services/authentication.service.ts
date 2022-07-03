a69935a03b90381f15f9ecc66d2d15d32c08a854
BrendanMeins <brendan.27@hotmail.de>
2022-07-03 14:29:11 +0200

    added interceptor

--- frontend/src/app/services/authentication.service.ts
+++ frontend/src/app/services/authentication.service.ts
@@ -80,6 +80,14 @@ export class AuthenticationService {
     return true
   }
 
+  public getToken() : string {
+    const token = localStorage.getItem('token');
+    if (token == null){
+      return ""
+    }
+    return token
+  }
+
 
 
 
