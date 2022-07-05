1cae4cafcadbe28a4bbc9e52250d544d1a1679f3
Leantar <t.peters@mailbox.org>
2022-07-05 16:45:01 +0200

    test

--- enclavedummy/main.go
+++ enclavedummy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"net/http"
@@ -113,11 +114,49 @@ func main() {
 	e.GET("/login/initialize", BeginLoginHandler)
 	e.POST("/login/finalize", FinishLoginHandler)
 
+	e.HTTPErrorHandler = customHTTPErrorHandler
+
 	err = e.Start(":2533")
 	if err == http.ErrServerClosed {
 		err = nil
 	}
 	if err != nil {
 		panic(err)
+	}
+}
+
+func customHTTPErrorHandler(err error, c echo.Context) {
+	log.Printf("i have encountered an error: %v", err)
+
+	if c.Response().Committed {
+		return
+	}
+
+	he, ok := err.(*echo.HTTPError)
+	if ok {
+		if he.Internal != nil {
+			if herr, ok := he.Internal.(*echo.HTTPError); ok {
+				he = herr
+			}
+		}
+	} else {
+		he = &echo.HTTPError{
+			Code:    http.StatusInternalServerError,
+			Message: http.StatusText(http.StatusInternalServerError),
+		}
+	}
+
+	// Issue #1426
+	code := he.Code
+	message := he.Message
+	if m, ok := he.Message.(string); ok {
+		message = echo.Map{"message": fmt.Sprintf("Houston, we have a problem!: %s", m), "error": err.Error()}
+	}
+
+	// Send response
+	if c.Request().Method == http.MethodHead { // Issue #608
+		err = c.NoContent(he.Code)
+	} else {
+		err = c.JSON(code, message)
 	}
 }
