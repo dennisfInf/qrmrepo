88e2891a13de2283ce0cf923b8eb10f0b5704aa9
BrendanMeins <brendan.27@hotmail.de>
2022-12-07 15:15:56 +0100

    cleanup

--- relay/models/lookup.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Lookup struct {
-	ID             uint64 `db:"id"`
-	Username       string `db:"username"`
-	EnclaveAddress string `db:"enclave_address"`
-}
--- relay/persistence/schema.go
+++
@@ -1,9 +0,0 @@
-package persistence
-
-var schemas = [...]string{
-	`CREATE TABLE IF NOT EXISTS Lookup (
-		id BIGSERIAL PRIMARY KEY,
-		username TEXT UNIQUE NOT NULL,
-		enclave_address TEXT UNIQUE NOT NULL
-	);`,
-}
